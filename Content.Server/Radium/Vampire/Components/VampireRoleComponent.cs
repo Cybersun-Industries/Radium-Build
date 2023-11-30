3de9c47702a5316dfe4a02b6c779db810e60308f
KoorrLin <KoorrLin@mail.ru>
2023-11-30 18:05:58 +0300

    Vampire Init

---
+++ Content.Server/Radium/Vampire/Components/VampireRoleComponent.cs
@@ -0,0 +1,8 @@
+using Content.Shared.Roles;
+
+namespace Content.Server.Radium.Vampire.Components;
+
+[RegisterComponent]
+public sealed partial class VampireRoleComponent : AntagonistRoleComponent
+{
+}
