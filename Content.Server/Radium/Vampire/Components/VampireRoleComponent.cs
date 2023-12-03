8a31a930fe6ad7ea944332657c265bc0bbf47037
freeze2222 <opop1094@gmail.com>
2023-12-03 07:51:02 +0300

    Backup

--- Content.Server/Radium/Vampire/Components/VampireRoleComponent.cs
+++
@@ -1,8 +0,0 @@
-using Content.Shared.Roles;
-
-namespace Content.Server.Radium.Vampire.Components;
-
-[RegisterComponent]
-public sealed partial class VampireRoleComponent : AntagonistRoleComponent
-{
-}
