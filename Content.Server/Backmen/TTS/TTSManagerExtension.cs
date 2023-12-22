90d912ef8a8c6eac10978fde7859091c3700f97b
freeze2222 <opop1094@gmail.com>
2023-12-22 07:14:01 +0300

    Fixes + TTS update

--- Content.Server/Backmen/TTS/TTSManagerExtension.cs
+++ Content.Server/Backmen/TTS/TTSManagerExtension.cs
@@ -84,7 +84,7 @@ public static class TTSManagerExtension
             ApiToken = token,
             Text = text,
             Speaker = speaker,
-            Effect = "Radio"
+            //Effect = "Radio" RADIUM
         };
 
         var reqTime = DateTime.UtcNow;
@@ -154,7 +154,7 @@ public static class TTSManagerExtension
             ApiToken = token,
             Text = text,
             Speaker = speaker,
-            Effect = "Echo"
+            //Effect = "Echo"
         };
 
         var reqTime = DateTime.UtcNow;
