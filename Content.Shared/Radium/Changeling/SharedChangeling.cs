8fee150fa7cf1bd98bd380171a1cdac723adb276
freeze2222 <opop1094@gmail.com>
2024-07-13 22:01:39 +0300

    Add changeling event dummies

--- Content.Shared/Radium/Changeling/SharedChangeling.cs
+++ Content.Shared/Radium/Changeling/SharedChangeling.cs
@@ -5,49 +5,74 @@ using Robust.Shared.Serialization;
 namespace Content.Shared.Radium.Changeling;
 
 [Serializable, NetSerializable]
-public sealed partial class SoulEvent : SimpleDoAfterEvent
-{
-}
+public sealed partial class SoulEvent : SimpleDoAfterEvent;
 
 [Serializable, NetSerializable]
-public sealed partial class HarvestEvent : SimpleDoAfterEvent
+public sealed partial class HarvestEvent : SimpleDoAfterEvent;
+
+
+public sealed class HarvestDoAfterComplete(EntityUid target) : EntityEventArgs
 {
+    public readonly EntityUid Target = target;
 }
 
+public sealed class HarvestDoAfterCancelled : EntityEventArgs;
 
-public sealed class HarvestDoAfterComplete : EntityEventArgs
-{
-    public readonly EntityUid Target;
+public sealed partial class ChangelingAbsorbDnaActionEvent : EntityTargetActionEvent;
 
-    public HarvestDoAfterComplete(EntityUid target)
-    {
-        Target = target;
-    }
-}
+public sealed partial class ChangelingShopActionEvent : InstantActionEvent;
 
-public sealed class HarvestDoAfterCancelled : EntityEventArgs
-{
-}
+public sealed partial class ChangelingStasisActionEvent : InstantActionEvent;
 
-public sealed partial class ChangelingAbsorbDnaActionEvent : EntityTargetActionEvent
-{
-}
+public sealed partial class ChangelingTransformActionEvent : InstantActionEvent;
 
-public sealed partial class ChangelingShopActionEvent : InstantActionEvent
-{
-}
+public sealed partial class ActionChangelingAdrenalineSacsEvent : InstantActionEvent;
 
-public sealed partial class ChangelingStasisActionEvent : InstantActionEvent
-{
-}
+public sealed partial class ActionChangelingStrainedMusclesEvent : InstantActionEvent;
 
-public sealed partial class ChangelingTransformActionEvent : InstantActionEvent
-{
-}
+public sealed partial class ActionChangelingAnatomicPanaceaEvent : InstantActionEvent;
 
-public sealed partial class ChangelingSpeedUpActionEvent : InstantActionEvent
-{
-}
+public sealed partial class ActionChangelingChitinousArmorEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingVoidAdaptationEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingOrganicShieldEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingMimicVoiceEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingLesserFormEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingFleshmendEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingChameleonSkinEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingBiodegradeEvent : InstantActionEvent;
+
+public sealed partial class PassiveChangelingAugmentedEyesightEvent : InstantActionEvent; //TODO: Toggleable action
+
+public sealed partial class PassiveChangelingDefibrillatorGraspEvent : InstantActionEvent; //TODO: Toggleable action
+
+public sealed partial class ActionChangelingDnaStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingTransformationStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingMuteStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingBlindStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingHallutinationStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingCryogenicStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingFalseArmbladeStingEvent : EntityTargetActionEvent;
+
+public sealed partial class ActionChangelingArmBladeEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingResonantShriekEvent : InstantActionEvent;
+
+public sealed partial class ActionChangelingDissonantShriekEvent : InstantActionEvent;
+
+
 
 [NetSerializable, Serializable]
 public enum ChangelingVisuals : byte
